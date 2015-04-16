baa0450e840327d353963a03857b8e23591315f1
herlesupreeth <herlesupreeth@gmail.com>
2015-04-16 08:24:31 +0300

    first commit with entire website

---
+++ example/trivia.js
@@ -0,0 +1,62 @@
+var GLOBAL_ACTIONS = {
+    'play': function () {
+        wavesurfer.playPause();
+    },
+
+    'back': function () {
+        wavesurfer.skipBackward();
+    },
+
+    'forth': function () {
+        wavesurfer.skipForward();
+    },
+
+    'toggle-mute': function () {
+        wavesurfer.toggleMute();
+    },
+
+    'play2': function () {
+        wavesurfer2.playPause();
+    },
+
+    'back2': function () {
+        wavesurfer2.skipBackward();
+    },
+
+    'forth2': function () {
+        wavesurfer2.skipForward();
+    },
+
+    'toggle-mute2': function () {
+        wavesurfer2.toggleMute();
+    }
+};
+
+
+// Bind actions to buttons and keypresses
+document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('keydown', function (e) {
+        var map = {
+            32: 'play',       // space
+            37: 'back',       // left
+            39: 'forth'       // right
+        };
+        var action = map[e.keyCode];
+        if (action in GLOBAL_ACTIONS) {
+            if (document == e.target || document.body == e.target) {
+                e.preventDefault();
+            }
+            GLOBAL_ACTIONS[action](e);
+        }
+    });
+
+    [].forEach.call(document.querySelectorAll('[data-action]'), function (el) {
+        el.addEventListener('click', function (e) {
+            var action = e.currentTarget.dataset.action;
+            if (action in GLOBAL_ACTIONS) {
+                e.preventDefault();
+                GLOBAL_ACTIONS[action](e);
+            }
+        });
+    });
+});
